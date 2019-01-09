ee9f69b19b2560c8b6f96157bd29f81817e24dac
yinzitang <328688853@qq.com>
2019-01-09 10:18:37 +0800

    19-1-9

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn234423 React
+            Learn React
             456464654654
           </a>
         </header>
