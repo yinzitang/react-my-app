55d2b775dca381116882ca3566cba02c887cdd24
yinzitang <328688853@qq.com>
2019-03-25 19:48:03 +0800

    练习

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tab from './tab'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Tab></Tab>
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/tab.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Tab extends Component {
+    render () {
+        return (
+            <div className="tab">
+                <div>12123</div>
+                <div>32333</div>
+                <div>56656</div>
+            </div>
+        )
+    }
+}
+export default Tab;
